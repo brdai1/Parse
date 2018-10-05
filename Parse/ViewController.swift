929e392768ceb7b4b8b5cbffa2ae05ca67d9acc3
user143116 <user143116@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-10-05 07:49:31 -0700

    Initial Commit

---
+++ Parse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parse
+//
+//  Created by user143116 on 10/5/18.
+//  Copyright © 2018 MSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
